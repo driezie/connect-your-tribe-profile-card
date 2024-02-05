b15a925952423caa38a3d14293092fdd442a868c
Jelte (aka Driezie) <80174866+driezie@users.noreply.github.com>
2024-02-05 15:10:25 +0100

    ft: added default demo for profile-card
    Dag 1 | Sprint 7 | Datum 05-02-24
    - Api connection
    - Basic demo
    - Working loop system for socials and content
    
    Voor meer informatie bekijk:
    https://github.com/driezie/connect-your-tribe-profile-card/wiki/Werklog-%F0%9F%93%85#dag-1--sprint-7--datum-05-02-24

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+function turn() {
+//   toggle card id to be turned
+    document.getElementById("card").classList.toggle("turned");
+}
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ import express from 'express'
 import fetchJson from './helpers/fetch-json.js'
 
 // Haal data op uit de FDND API, ga pas verder als de data gedownload is
-const data = await fetchJson('https://fdnd.directus.app/items/person/65')
-// console.log(data); // uncomment om de opgehaalde data te checken
+const data = await fetchJson('https://fdnd.directus.app/items/person/46')
+console.log(JSON.parse(data.data.custom)); // uncomment om de opgehaalde data te checken
 
 // Maak een nieuwe express app aan
 const app = express()
@@ -21,8 +21,10 @@ app.use(express.static('public'))
 
 // Maak een GET route voor de index
 app.get('/', function (request, response) {
+  // Haalt de custom data op die ik heb aangemaakt in de FDND API
+  const custom = JSON.parse(data.data.custom)
   // Render index.ejs uit de views map en geef uit FDND API opgehaalde data mee
-  response.render('index', data)
+  response.render('index', {data: data.data, custom})
 })
 
 // Maak een POST route voor de index
@@ -37,5 +39,11 @@ app.set('port', process.env.PORT || 8000)
 // Start express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
+  console.log(`-----`)
+  console.log(``)
+  console.log(`Applicatie is gestart via link http://localhost:${app.get('port')}`)
+  console.log(``)
+  console.log(`Deze applicatie is gemaakt door Jelte Cost`)
+  console.log(``)
+  console.log(`-----`)
 })
