dec00637fa2b04b4b7287bc38c856d5091a53a8e
Jelte (aka Driezie) <80174866+driezie@users.noreply.github.com>
2024-02-07 10:03:00 +0100

    fix: added Attribute inert for focus fix
     [JS] inert toggle toegevoegd aan de javascript bestand bij het draaien van het kaartje
     [CSS] pointer-events verwijderd omdat dit iets was wat ik niet nodig had, maar die inert

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,6 @@
 function turn() {
-//   toggle card id to be turned
-    document.getElementById("card").classList.toggle("turned");
+    document.getElementById("card").classList.toggle("turned");    
+    // This toggles the inert attribute on the back and front of the card to be active for a focus
+    document.querySelector(".back").toggleAttribute("inert");
+    document.querySelector(".front").toggleAttribute("inert");
 }
