b3cccb0acf84222872841d50559ceaf08dd2d5ed
Jelte (aka Driezie) <80174866+driezie@users.noreply.github.com>
2024-02-07 15:15:11 +0100

    ft: added animations

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,45 @@ function turn() {
     document.querySelector(".back").toggleAttribute("inert");
     document.querySelector(".front").toggleAttribute("inert");
 }
+
+function iniFollowCursor() {
+	// de te volgen elementen opzoeken --> de li's
+	elements = document.querySelectorAll("body");
+	
+	// elke li in de array laten luisteren naar mousemoves
+	elements.forEach( (element) => {
+		element.addEventListener("mousemove", updateMouseLocalCoor);
+	});
+}
+
+
+function updateMouseLocalCoor(e){
+	let mouseX = e.clientX;
+	let mouseY = e.clientY;
+	
+	const element = e.currentTarget;
+	const elementRectangle = element.getBoundingClientRect();
+	
+	let elementWidth = elementRectangle.width;
+	let elementHeight = elementRectangle.height;
+    
+	
+	// de top en left opslaan
+	let elementLeft = elementRectangle.left;
+	let elementTop = elementRectangle.top;
+	
+	let elementMouseX = mouseX - elementLeft;
+	let x = elementMouseX / elementWidth;
+	
+	// zelfde voor y
+	let elementMouseY = mouseY - elementTop;
+	let y = elementMouseY / elementHeight;
+	
+    console.log(x * 100);
+    console.log(y * 100);
+	element.style.setProperty("--element-mouse-x", x * 100);
+	element.style.setProperty("--element-mouse-y", y * 100);
+}
+
+// het volgen starten
+iniFollowCursor();
